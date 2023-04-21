ebb7685bac400894207d410dd47e8e7a34c83753
jcbraz <josecbraz@outlook.com>
2023-04-21 16:39:10 +0100

    Rita paixao leva no bujao

--- src/lexer/_new_lexer.py
+++ src/lexer/_new_lexer.py
@@ -1,24 +1,24 @@
 import ply.lex as lex
 
-class TableControl:
-    def __init__(self):
-        self.indentifier_control = 0
+# class TableControl:
+#     def __init__(self):
+#         self.indentifier_control = 0
 
-    def increment(self):
-        self.indentifier_control += 1
+#     def increment(self):
+#         self.indentifier_control += 1
 
-    def decrement(self):
-        self.indentifier_control -= 1
+#     def decrement(self):
+#         self.indentifier_control -= 1
 
-    def getIdentifierControl(self):
-        return self.indentifier_control
+#     def getIdentifierControl(self):
+#         return self.indentifier_control
     
-    def setter(self, value):
-        self.indentifier_control = value
+#     def setter(self, value):
+#         self.indentifier_control = value
         
-    def get_table_title(self,t):
-        title = t.value.split('.')
-        return title
+#     def get_table_title(self,t):
+#         title = t.value.split('.')
+#         return title
     
 states = (
     ("array", "inclusive"),
@@ -33,21 +33,22 @@ tokens = (
     'INTEGER',
     'FLOAT',
     'BOOLEAN',
+    'DATE',
     'LBRACKET',
     'RBRACKET',
     'LCURLY',
     'RCURLY',
     'COMMA',
     'DOT',
+    'SPACE',
     'NEWLINE',
     'COMMENT',
-    'DATE',
     'TABLE_HEADER',
     'CHILD_HEADER',
     'ARRAY_TABLES_HEADER',
 )
 
-table_control = TableControl()
+# table_control = TableControl()
 
 t_EQUAL = r'='
 t_LBRACKET = r'\['
@@ -56,6 +57,7 @@ t_LCURLY = r'\{'
 t_RCURLY = r'\}'
 t_COMMA = r','
 t_DOT = r'\.'
+t_SPACE = r'\s+'
 t_ignore = ' \t'
 
 # Add support for comments
@@ -80,7 +82,6 @@ def t_ANY_VARIABLE(t):
 
 def t_ANY_STRING(t):
     r'"([^"\\]|\\.)*"'
-    t.value = t.value[1:-1]
     return t
 
 def t_ANY_INTEGER(t):
@@ -105,38 +106,30 @@ def t_ANY_NEWLINE(t):
 
 def t_TABLE_HEADER(t):
     r'\[\w*\]\n*?'
-    t.value = t.value[1:-1]
     t.lexer.begin('table')
-    table_control.setter(1)
     return t
 
 def t_ARRAY_TABLES_HEADER(t):
     r'\[\[[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]*)*\]\]'
-    t.value = t.value[2:-2]
     t.lexer.begin('array')
     return t
 
 def t_table_TABLE_HEADER(t):
     r'\[\w*\]\n*?'
-    t.value = t.value[1:-1]
     return t
 
 def t_table_CHILD_HEADER(t):
     r'\[[a-zA-Z]+\.(?:[a-zA-Z]*\.)*[a-zA-Z]*\]'
-    t.value = t.value[1:-1]
     t.lexer.begin('child')
     return t
 
 def t_child_TABLE_HEADER(t):
     r'\[\w*\]\n*?'
-    t.value = t.value[1:-1]
     t.lexer.begin('table')
-    table_control.setter(1)
     return t
 
 def t_child_CHILD_HEADER(t):
     r'\[[a-zA-Z]+\.(?:[a-zA-Z]*\.)*[a-zA-Z]*\]'
-    t.value = t.value[1:-1]
     return t
 
 def t_ANY_error(t):
@@ -145,34 +138,34 @@ def t_ANY_error(t):
 
 lexer = lex.lex()
 
-data = """
-# This is a TOML document. Boom.
-title = "TOML Example"
+# data = """
+# # This is a TOML document. Boom.
+# title = "TOML Example"
 
-[owner]
-name = "Tom Preston-Werner"
-dob = 1979-05-27T07:32:00-08:00
+# [owner]
+# name = "Tom Preston-Werner"
+# dob = 1979-05-27T07:32:00-08:00
 
-[database]
-enabled = true
-ports = [ 8000, true, 8002 ]
-data = [ ["delta", "phi"], [3.14] ]
-temp_targets = { cpu = 79.5, case = 72.0 }
+# [database]
+# enabled = true
+# ports = [ 8000, true, 8002 ]
+# data = [ ["delta", "phi"], [3.14] ]
+# temp_targets = { cpu = 79.5, case = 72.0 }
 
-[servers]
+# [servers]
 
-[servers.alpha]
-ip = "10.0.0.1"
-role = "frontend"
+# [servers.alpha]
+# ip = "10.0.0.1"
+# role = "frontend"
 
-[servers.beta]
-ip = "10.0.0.2"
-role = "backend"
+# [servers.beta]
+# ip = "10.0.0.2"
+# role = "backend"
 
-[[products.dizes.dizes]]
-"""
+# [[products.dizes.dizes]]
+# """
 
-lexer.input(data)
+# lexer.input(data)
 
-while tok := lexer.token():
-    print(tok)
+# while tok := lexer.token():
+#     print(tok)
---
+++ src/lexer/_parser.py
@@ -0,0 +1,133 @@
+import ply.yacc as yacc
+import json
+
+# Import the tokens list from _new_lexer.py
+from _new_lexer import tokens
+
+
+def p_document(p):
+    '''document : toml
+                | toml document'''
+    if len(p) == 3:
+        p[0] = p[1] + p[2]
+    else:
+        p[0] = p[1]
+
+
+def p_toml(p):
+    '''toml : content
+            | array
+            | table
+            | inline_table
+            | array_tables'''
+    p[0] = p[1]
+
+
+def p_array_tables(p):
+    '''array_tables : ARRAY_TABLES_HEADER content
+                    | ARRAY_TABLES_HEADER array_tables'''
+    p[0] = (p[1], p[2])
+
+
+def p_child(p):
+    '''child : CHILD_HEADER content
+             | CHILD_HEADER child'''
+    p[0] = (p[1], p[2])
+
+
+def p_inline_table(p):
+    '''inline_table : LCURLY content RCURLY
+                    | LCURLY RCURLY'''
+    if len(p) == 4:
+        p[0] = p[2]
+    else:
+        p[0] = []
+
+
+def p_table(p):
+    '''table : TABLE_HEADER content
+             | TABLE_HEADER child'''
+    p[0] = (p[1], p[2])
+
+
+def p_content(p):
+    '''content : assignment
+               | content assignment
+               |'''  # empty
+    if len(p) == 3:
+        p[0] = p[1] + [p[2]]
+    elif len(p) == 2:
+        p[0] = [p[1]]
+
+
+def p_assignment(p):
+    '''assignment : VARIABLE EQUAL value
+                  | VARIABLE EQUAL array'''
+    p[0] = (p[1], p[3])
+
+
+def p_array(p):
+    '''array : LBRACKET values RBRACKET
+             | LBRACKET array RBRACKET
+             | LBRACKET RBRACKET'''
+    if len(p) == 4:
+        p[0] = p[2]
+    else:
+        p[0] = []
+
+
+def p_values(p):
+    '''values : value COMMA values
+              | value'''
+    if len(p) == 4:
+        p[0] = [p[1]] + p[3]
+    else:
+        p[0] = [p[1]]
+
+
+def p_value(p):
+    '''value : STRING
+             | INTEGER
+             | FLOAT
+             | BOOLEAN
+             | DATE'''
+    p[0] = p[1]
+
+
+def p_error(p):
+    if p:
+        print(f"Syntax error at '{p.value}', line {p.lineno}")
+    else:
+        print("Syntax error at EOF")
+
+
+parser = yacc.yacc()
+
+data = """
+# This is a TOML document. Boom.
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00-08:00
+
+[database]
+enabled = true
+ports = [ 8000, 8001, 8002 ]
+data = [ ["delta", "phi"], [3.14] ]
+temp_targets = { cpu = 79.5, case = 72.0 }
+
+[servers]
+
+[servers.alpha]
+ip = "10.0.0.1"
+role = "frontend"
+
+[servers.beta]
+ip = "10.0.0.2"
+role = "backend"
+"""
+
+
+result = parser.parse(data)
+print(parser.parse(data))
