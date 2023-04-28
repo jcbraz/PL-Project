2e50b0087009848981ceeb6a69da29ce3015197e
jcbraz <josecbraz@outlook.com>
2023-04-28 10:57:40 +0100

    Add requirements file

--- tomljson/__init__.py
+++ tomljson/__init__.py
@@ -1,3 +1,3 @@
 # source .venv/bin/activate
 # export PYTHONPATH=$PYTHONPATH:/Users/jcbraz/University/3A2S/PL/PL-Project/tomljson
-# run => python -m pip install tomljson
+# python -m pip install tomljson
